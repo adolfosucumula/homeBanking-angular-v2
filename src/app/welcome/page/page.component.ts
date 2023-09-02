0bd11094364b928ba17c1b20b246167952b37d8d
adolfosucumula <rocketmc2009@gmail.com>
2023-09-02 15:52:38 +0100

    sets of sidenav media screen

--- src/app/welcome/page/page.component.ts
+++ src/app/welcome/page/page.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { Component, Input, Output, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-page',
@@ -10,6 +12,32 @@ export class PageComponent {
   public isVisited = true;
   isLogged = false;
 
+  //ViewChild is a method to catch the sidenav class
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+
   @Input() page: string = 'Home'
+
+  constructor(private observer: BreakpointObserver){
+
+  }
+
+
+  /**
+   * Method to catch the page resize event
+   * Check if the page size matches. If true change the sidenav mode to
+   * 'over' and close it, if false open it setting to 'side' mode
+   */
+  ngAfterViewInit(){
+    this.observer.observe(['(max-width: 800px)']).subscribe((res)=>{
+
+      if(res.matches){
+        this.sidenav.mode = 'over'
+        this.sidenav.close()
+      }else{
+        this.sidenav.mode = 'side'
+        this.sidenav.open()
+      }
+    })
+  }
 
 }
