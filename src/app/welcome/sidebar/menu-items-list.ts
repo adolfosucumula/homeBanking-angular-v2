c259da28eb27e4c28241561c53f54b71d67a4fc7
adolfosucumula <rocketmc2009@gmail.com>
2023-09-02 14:56:32 +0100

    sidenav created

---
+++ src/app/welcome/sidebar/menu-items-list.ts
@@ -0,0 +1,38 @@
+
+export const menuItemsList = [
+  {
+    id: 1,
+    router: "",
+    label: "Home",
+    show: true,
+    icon: "home"
+  },
+  {
+    id: 1,
+    router: "/dashboard",
+    label: "Dashboard",
+    show: true,
+    icon: "dashboard"
+  },
+  {
+    id: 1,
+    router: "/balance",
+    label: "My Balance",
+    show: true,
+    icon: "money"
+  },
+  {
+    id: 1,
+    router: "/send-money",
+    label: "Send money",
+    show: true,
+    icon: "home"
+  },
+  {
+    id: 1,
+    router: "/credit-account",
+    label: "Credit Account",
+    show: true,
+    icon: "home"
+  },
+]
--- src/app/welcome/sidebar/sidebar.component.ts
+++ src/app/welcome/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { menuItemsList } from './menu-items-list';
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent {
-
+  menuItems = menuItemsList
+  userImg = 'https://media.licdn.com/dms/image/D4D03AQEnQcEH1eGdeQ/profile-displayphoto-shrink_800_800/0/1672836007935?e=2147483647&v=beta&t=Sfx47qVeN9ca7HH1fuuwoYNSEDyoTuXfNYn-Gf5symo'
+  username = 'Admin'
 }
